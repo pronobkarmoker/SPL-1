302d59107dbb2cb87e6c743e0e72fa6613c125eb
Pronob <pronobkarmoker1@gmail.com>
2023-09-13 22:13:23 +0600

    Hellinger distance practice code

---
+++ Hellinger_distance&InformationGain.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <math.h>
+
+#define NUM_SAMPLES 4
+#define NUM_CLASSES 2
+#define NUM_FEATURES 3
+
+// Function to calculate Hellinger Distance between two class distributions
+double calculateHellingerDistance(double p[], double q[], int numClasses) {
+    double distance = 0.0;
+
+    for (int i = 0; i < numClasses; i++) {
+        double rootP = sqrt(p[i]);
+        double rootQ = sqrt(q[i]);
+        distance += pow(rootP - rootQ, 2);
+    }
+
+    return sqrt(0.5 * distance);
+}
+
+// Function to calculate Information Gain using Hellinger Distance for a specific class
+double calculateInformationGainForClass(double classDistr[][NUM_CLASSES], int numSamples, int numClasses, int classIndex) {
+    double totalClassDistr[NUM_CLASSES] = {0};
+    double leftClassDistr[NUM_CLASSES] = {0};
+    double rightClassDistr[NUM_CLASSES] = {0};
+
+    // Calculate total class distribution
+    for (int i = 0; i < numSamples; i++) {
+        for (int j = 0; j < numClasses; j++) {
+            totalClassDistr[j] += classDistr[i][j];
+        }
+    }
+
+    // Calculate Hellinger Distance for left branch (in this case, the same as totalClassDistr)
+    for (int j = 0; j < numClasses; j++) {
+        leftClassDistr[j] = totalClassDistr[j];
+    }
+
+    // Calculate Hellinger Distance for right branch (empty)
+    for (int j = 0; j < numClasses; j++) {
+        rightClassDistr[j] = 0;
+    }
+
+    double totalHellingerDistance = calculateHellingerDistance(totalClassDistr, totalClassDistr, numClasses);
+    double leftHellingerDistance = calculateHellingerDistance(totalClassDistr, leftClassDistr, numClasses);
+    double rightHellingerDistance = calculateHellingerDistance(totalClassDistr, rightClassDistr, numClasses);
+
+    double pLeft = (double) numSamples / (2.0 * NUM_SAMPLES); // Adjust the weight of the left branch
+    double pRight = 1.0 - pLeft; // Adjust the weight of the right branch
+
+    double informationGain = totalHellingerDistance - ((pLeft * leftHellingerDistance) + (pRight * rightHellingerDistance));
+
+    return informationGain;
+}
+
+int main() {
+    int data[NUM_SAMPLES][NUM_FEATURES] = {
+        {5, 10, 15},
+        {50, 55, 60},
+        {100, 150, 200},
+        {250, 300, 350}
+    };
+
+    int class_labels[NUM_SAMPLES] = {1, 1, 2, 2};
+
+    double classDistr[NUM_SAMPLES][NUM_CLASSES] = {0};
+
+    // Convert data into class distributions
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        classDistr[i][class_labels[i] - 1] = 1; // Assign 1 to the corresponding class
+    }
+
+    // Calculate Information Gain for each class
+    for (int c = 0; c < NUM_CLASSES; c++) {
+        double infoGain = calculateInformationGainForClass(classDistr, NUM_SAMPLES, NUM_CLASSES, c);
+        printf("Information Gain for Class %d: %lf\n", c + 1, infoGain);
+    }
+
+    return 0;
+}
--- Hellinger_distance.c
+++ Hellinger_distance.c
@@ -1,58 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-double hellinger_distance(int data[][3], int class_labels[], int n_classes)
+// Function to calculate Hellinger Distance
+double calculateHellingerDistance(double p[], double q[], int n)
 {
-    double p[n_classes][3];
-    double q[n_classes][3];
-    double distance, sqrt_p, sqrt_q;
-    int i, j;
-
-    // Initialize p and q.
-    for (i = 0; i < n_classes; i++)
+    double distance = 0.0;
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < 3; j++)
-        {
-            p[i][j] = 0.0;
-            q[i][j] = 0.0;
-        }
+        double sqrt_p = sqrt(p[i]);
+        double sqrt_q = sqrt(q[i]);
+        distance += (sqrt_p - sqrt_q) * (sqrt_p - sqrt_q);
     }
-
-    // Calculate p.
-    for (i = 0; i < n_classes; i++)
-    {
-        for (j = 0; j < n_classes; j++)
-        {
-            if (class_labels[j] == i)
-            {
-                for (int k = 0; k < 3; k++)
-                {
-                    p[i][k] += data[j][k];
-                }
-            }
-        }
-    }
-
-    // Calculate q.
-    for (i = 0; i < n_classes; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            q[i][j] = sqrt(p[i][j]);
-        }
-    }
-
-    // Calculate the Hellinger distance.
-    distance = 0.0;
-    for (i = 0; i < n_classes; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            distance += (q[i][j] - p[i][j]) * (q[i][j] - p[i][j]);
-        }
-    }
-
-    return 0.5 * distance;
+    distance = sqrt(0.5 * distance);
+    return distance;
 }
 
 int main()
@@ -64,8 +24,40 @@ int main()
         {250, 300, 350}};
 
     int class_labels[4] = {1, 1, 2, 2};
+    int num_samples = 4;
+    int num_features = 3;
+    int num_classes = 2;
 
-    double distance = hellinger_distance(data, class_labels, 2);
+    // Calculate class-wise counts
+    int class_counts[num_classes] ; 
+    class_counts[num_classes] = 0;
+    for (int i = 0; i < num_samples; i++)
+    {
+        class_counts[class_labels[i] - 1]++;
+    }
 
-    printf("%ld", distance);
+    // Calculate probability distributions for each class
+    double class_probs[num_classes][num_features];
+    for (int i = 0; i < num_classes; i++)
+    {
+        for (int j = 0; j < num_features; j++)
+        {
+            int feature_sum = 0;
+            for (int k = 0; k < num_samples; k++)
+            {
+                if (class_labels[k] == i + 1)
+                {
+                    feature_sum += data[k][j];
+                }
+            }
+            class_probs[i][j] = (double)feature_sum / class_counts[i];
+        }
+    }
+
+    // Calculate Hellinger Distance between class probability distributions
+    double hellinger_distance = calculateHellingerDistance(class_probs[0], class_probs[1], num_features);
+
+    printf("Hellinger Distance: %lf\n", hellinger_distance);
+
+    return 0;
 }
